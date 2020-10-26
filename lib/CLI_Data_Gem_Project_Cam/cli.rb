8ffd4f288901c311c09397f2533e2ea32eb7761e
clone18476 <cameroncousins18@gmail.com>
2020-10-26 18:30:19 -0500

    Last commit before evaluation, all errors supposedly fixed, still needs refactoring for code noise

--- lib/CLI_Data_Gem_Project_Cam/cli.rb
+++ lib/CLI_Data_Gem_Project_Cam/cli.rb
@@ -6,44 +6,47 @@ module CLI_Data_Gem_Project_Cam
             puts "Welcome to the Anime Finder App!"
             puts "This app is used to help the user find new anime's to watch based on the chosen genre!"
             ask_genre
-            input = gets.strip.to_i
-            validate_input(input)
-            next_move(input)
+            input = "s"
+            until input == "n"
+                puts "Would you like to find another list of anime? (y/n)"
+                input = gets.strip.downcase
+                if input != "n"
+                    next_move(input)
+                end                  
+            end            
+            puts "Have a nice day and thank you for using the Anime Finder App!"
+            exit
         end
     
         def ask_genre
             puts ""
             puts "This is a list of the available genres to choose from:"
-            puts "Action, Adventure, Comedy, Drama, Slice of Life, Fantasy, Magic, Supernatural, Horror, Myster, Psychological, Romance, and Sci-Fi"
+            puts "Action, Adventure, Comedy, Drama, Slice of Life, Fantasy, Magic, Supernatural, Horror, Mystery, Psychological, Romance, and Sci-Fi"
             puts ""
             puts "Please enter the genre of the anime(s) you would like to discover!"
-            input = gets.strip.to_i
+            input = gets.strip
             genre = validate_input(input)
             api = Scraper.new(genre)
             api.fetch_data
-
         end
     
     
         def validate_input(input)
-            until input = "action" || "adventure" || "comedy" || "drama" || "slice of life" || "fantasy" || "magic" || "supernatural" || "horror" || "mystery" || "psychological" || "romance" || "sci-fi"
+            genre_array = ["action", "adventure", "comedy", "drama", "slice of life", "fantasy", "magic", "supernatural", "horror", "mystery", "psychological", "romance", "sci-fi"]
+            until genre_array.include? input.downcase
                 puts "Please enter a valid genre"
-                input = gets.strip.to_i
+                input = gets.strip
             end
             input
         end
 
         def next_move(input)
-            puts "Would you like to find another anime? (y/n)"
-            input = gets.strip.to_i
-            if input == "n" 
+            if input == "y" 
                 ask_genre
             else 
-                exit
+                puts "Please enter a valid response!"
             end
-          
         end
-        
     end
 end 
 
